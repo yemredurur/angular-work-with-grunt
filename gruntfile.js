2d48067b5faa960b3786a48c8d5a20b43985ccba
Yusuf Emre Durur <ydurur@ebay.com>
2016-02-21 18:41:11 +0200

    gruntfile updated

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,26 @@
 
 module.exports = function(grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+
+        cssmin: {
+            my_target: {
+                /*combine: {
+                    files: {
+                        'css/base.css' : ['css/base.css', 'css/bootstrap.min.css','css/bootstrap-theme.min.css']
+                    }
+                },*/
+                files: [{
+                    expand: true,
+                    cwd:  'css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'css/',
+                    ext: '.min.css'
+                }]
+            }
+        }
 
     });
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 };
