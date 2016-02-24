9ee65fd1fe2b3133782adc4e594b9fafd1991afb
Yusuf Emre Durur <ydurur@ebay.com>
2016-02-24 22:37:56 +0200

    grunt injector bug fix

--- gruntfile.js
+++ gruntfile.js
@@ -2,7 +2,7 @@
 module.exports = function(grunt) {
     var config = {
         app: 'app',
-        sourceBase: '/source/',
+        sourceBase: 'source/',
         source: 'source/app/',
         sourceJS: 'source/app/js',
         sourceCSS: 'source/app/css',
@@ -13,8 +13,106 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         config: config,
 
-        notify: {
+        concat: {
+            options: {
+                seperator: ';',
+                stripBanners: true,
+                banner: '/*! <%= pkg.name %> - v<% pkg.version %> -' + '<%= grunt.template.today("dd-mm-yyyy") %> */'
+            },
+            dist: {
+                src : ['css/base.css', 'css/bootstrap.min.css', 'css/bootstrap-grunt theme.min.css'],
+                dest: '<%= config.sourceCSS %>/base.css'
+            }
+        },
 
+        uglify: {
+            options: {
+                manage: false
+                /*preserveComments: true*/
+            },
+            my_target: {
+                files: {
+                    '<%= config.sourceJS %>/test.min.js': ['js/app/app-add-user.js','js/app/app-user-list.js'],
+                    '<%= config.sourceJS %>/lib.min.js': ['js/lib/jquery.min.js','js/lib/angular.min.js','js/lib/angular-messages.js','js/lib/firebase.js','js/lib/angularfire.min.js','js/lib/bootstrap.min.js']
+                }
+            }
+        },
+
+        cssmin: {
+            my_target: {
+                options: {
+                    preserveComments: true
+                },
+                files: [{
+                    expand: true,
+                    cwd:  '<%= config.sourceCSS %>',
+                    src: ['*.css', '!*.min.css'],
+                    dest: '<%= config.sourceCSS %>',
+                    ext: '.min.css'
+                }]
+            }
+        },
+
+        sass: {
+            dist: {
+                files: {
+                    '<%= config.sourceSASS %>/sass-test.css': 'sass/test.scss'
+                }
+            }
+        },
+
+        copy: {
+            /*html: {
+             src: '<%= config.app %>/*.html',
+             dest: '<%= config.source %>/index.html'
+             },*/
+            main: {
+                files: [
+                    {expand: true, src: ['<%= config.app %>/*'], dest: 'source/', filter: 'isFile'}
+                ]
+            }
+        },
+
+        injector: {
+            options: {
+                addRootSlash: false,
+                ignorePath:'<%= config.sourceBase %>',
+                template: '<%= config.app %>/index.html'
+            },
+            local_dependencies: {
+                files: {
+                    '<%= config.source %>/*': ['<%= config.sourceJS %>/*.js', '<%= config.sourceCSS %>/*.css']
+                }
+            }
+        },
+
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        '<%= config.sourceCSS %>/*.css',
+                        '<%= config.sourceJS %>/*.js',
+                        '<%= config.source %>*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './<%= config.source %>'
+                }
+            }
+        },
+
+        watch: {
+            options:{
+                livereload: true
+            },
+            dev:{
+                files: ['sass/*.scss','/js/*.js','/css/*.css','<%= config.app %>/*.html'],
+                tasks: ['uglify','cssmin','copy:main','injector']
+            }
+        },
+
+        notify: {
             custom:{
                 options:{
                     enabled:true,
@@ -39,112 +137,13 @@ module.exports = function(grunt) {
             }
         },
 
-        cssmin: {
-            my_target: {
-                options: {
-                    preserveComments: true
-                },
-                files: [{
-                    expand: true,
-                    cwd:  '<%= config.sourceCSS %>',
-                    src: ['*.css', '!*.min.css'],
-                    dest: '<%= config.sourceCSS %>',
-                    ext: '.min.css'
-                }]
-            }
-        },
-
-        uglify: {
-            options: {
-                manage: false
-            /*preserveComments: true*/
-            },
-            my_target: {
-                files: {
-                    '<%= config.sourceJS %>/test.min.js': ['js/app/app-add-user.js','js/app/app-user-list.js'],
-                    '<%= config.sourceJS %>/lib.min.js': ['js/lib/jquery.min.js','js/lib/angular.min.js','js/lib/angular-messages.js','js/lib/firebase.js','js/lib/angularfire.min.js','js/lib/bootstrap.min.js']
-                }
-            }
-
-        },
-
-        concat: {
-            options: {
-                seperator: ';',
-                stripBanners: true,
-                banner: '/*! <%= pkg.name %> - v<% pkg.version %> -' + '<%= grunt.template.today("dd-mm-yyyy") %> */'
-            },
-            dist: {
-                src : ['css/base.css', 'css/bootstrap.min.css', 'css/bootstrap-grunt theme.min.css'],
-                dest: '<%= config.sourceCSS %>/base.css'
-            }
-        },
-
-        sass: {
-            dist: {
-                files: {
-                    '<%= config.sourceSASS %>/sass-test.css': 'sass/test.scss'
-                }
-            }
-        },
-
-        watch: {
-            sass: {
-                files: ['sass/*.scss'],
-                tasks: ['sass']
-            }
-        },
-
         uncss: {
             dist: {
                 files: {
                     '<%= config.sourceCSS %>/*.css': 'app/index.html'
                 }
             }
-        },
-
-        injector: {
-            options: {
-                addRootSlash: false,
-                ignorePath:'<%= config.sourceBase %>',
-                template: '<%= config.source %>',
-                templateString: '<%= config.source %>'
-            },
-            local_dependencies: {
-                files: {
-                    '<%= config.source %>/*': ['<%= config.sourceJS %>/*.js', '<%= config.sourceCSS %>/*.css']
-                }
-            }
-        },
-
-        browserSync: {
-            dev: {
-                bsFiles: {
-                    src : [
-                        '<%= config.sourceCSS %>/*.css',
-                        '<%= config.sourceJS %>/*.js',
-                        '<%= config.source %>/app/*.html'
-                    ]
-                },
-                options: {
-                    watchTask: true,
-                    server: './<%= config.source %>'
-                }
-            }
-        },
-
-        copy: {
-            /*html: {
-                src: '<%= config.app %>/*.html',
-                dest: '<%= config.source %>/index.html'
-            },*/
-            main: {
-                files: [
-                    {expand: true, src: ['<%= config.app %>/*'], dest: 'source/', filter: 'isFile'},
-                ]
-            }
         }
-
 
     });
 
@@ -160,7 +159,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
 
 
-    grunt.registerTask('dev',['concat','uglify','cssmin','sass','copy:main','injector','browserSync','watch']);
+    grunt.registerTask('dev',['concat','uglify','cssmin','injector','sass','copy:main','browserSync','watch']);
 
     grunt.task.run('notify');
 
